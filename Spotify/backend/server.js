7f799ecf9ee367801c65885ff1877c069bc4c4ce
DevashishSharma-codess <drsharma@enacton.email>
2026-08-14 11:23:45 +0530

    fix: refactor server initialization to use async/await for database connection

--- Spotify/backend/server.js
+++ Spotify/backend/server.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const connectDB = require('./src/db/db');
 const app = require('./src/app');
 
-
-connectDB();
-
-app.listen(3000 , ()=>{
+(async () => {
+  await connectDB();
+  
+  app.listen(3000, () => {
     console.log('Server is running on port 3000')
-})
+  })
+})();
